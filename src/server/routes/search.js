84553e5a743bed07e1533bbb69c82733b0ac2b7b
WonJoon <seowj@gmail.com>
2022-02-11 20:33:58 +0900

    연결중

--- src/server/routes/search.js
+++ src/server/routes/search.js
@@ -10,18 +10,34 @@ router.get('/', (req, res) => {
 
     var api_url = 'https://openapi.naver.com/v1/search/book.json?query=' + encodeURI(req.query.search); // json 결과
     var request = require('request');
-    var options = {
-        url: api_url,
-        headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_sec}
-    };
-    request.get(options, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            res.render('search', {body});
-        } else {
-            res.status(response.statusCode).end();
-            console.log('error = ' + response.statusCode);
+    const options = [
+        {
+            url: api_url+'&sort=sim',
+            headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_sec}
+        },
+        {
+            url: api_url+'&sort=date',
+            headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_sec}
+        },
+        {
+            url: api_url+'&sort=count',
+            headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_sec}
         }
-    });
+    ]
+    let objList = [];
+    for(var i=0; i<3; i++){
+        request.get(options[i], function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                let obj = JSON.parse(body).items;
+                objList.push(obj);
+                console.log(objList);
+            } else {
+                res.status(response.statusCode).end();
+                console.log('error = ' + response.statusCode);
+            }
+        });
+    }
+    res.end();
 });
 
 module.exports = router;
